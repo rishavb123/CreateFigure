d0093b21605d53db713ad3b1d453e1001f11b168
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2019-09-22 18:19:19 -0400

    Inital Commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import os
+import sys
+import pyperclip
+
+os.system("touch " + sys.argv[1])
+os.system("%windir%\system32\mspaint.exe " + sys.argv[1])
+pyperclip.copy('\\begin{center}\n\t\\includegraphic[width=200pt]{' + sys.argv[1] + '}\n\\end{center}')
